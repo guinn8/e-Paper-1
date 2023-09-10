69166a0b1f8e905904bbb1505b83d878c8f22510
Gavin Guinn <gavinguinn1@gmail.com>
2023-09-10 20:54:35 +0100

    fixed the number box logic

--- RaspberryPi_JetsonNano/c/examples/EPD_2in13_V3_test.c
+++ RaspberryPi_JetsonNano/c/examples/EPD_2in13_V3_test.c
@@ -30,17 +30,20 @@
 #include "EPD_Test.h"
 #include "EPD_2in13_V3.h"
 #include <time.h> 
+#include <assert.h>  // For assert() function
 
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
+// Initialize E-Paper Display (EPD)
 void initEPD() {
     Debug("EPD_2in13_V3_test Demo\r\n");
     if(DEV_Module_Init() != 0) {
-        exit(-1); // exit if initialization fails
+        exit(-1);
     }
-    Debug("e-Paper Init and Clear...\r\n");
     EPD_2in13_V3_Init();
 }
 
+// Measure the time required to clear the EPD
 void measureClearTime() {
     struct timespec start = {0,0}, finish = {0,0};
     clock_gettime(CLOCK_REALTIME, &start);
@@ -49,6 +52,7 @@ void measureClearTime() {
     Debug("%ld S\r\n", finish.tv_sec - start.tv_sec);
 }
 
+// Create and configure image buffer
 UBYTE* createAndConfigImage() {
     UWORD Imagesize = ((EPD_2in13_V3_WIDTH % 8 == 0) ? (EPD_2in13_V3_WIDTH / 8) : (EPD_2in13_V3_WIDTH / 8 + 1)) * EPD_2in13_V3_HEIGHT;
     UBYTE *BlackImage = (UBYTE *)malloc(Imagesize);
@@ -61,79 +65,104 @@ UBYTE* createAndConfigImage() {
     return BlackImage;
 }
 
-void drawOnImage(UBYTE *BlackImage) {
-    Debug("Drawing\r\n");
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(WHITE);
-    Paint_DrawNum(0, 10, 123456789, &Font24, WHITE, BLACK);
-}
-
+// Finalize EPD and free resources
 void finalizeEPD(UBYTE *BlackImage) {
-    Debug("Goto Sleep...\r\n");
     EPD_2in13_V3_Sleep();
     free(BlackImage);
-    BlackImage = NULL;
     DEV_Delay_ms(2000);
-    Debug("close 5V, Module enters 0 power consumption ...\r\n");
     DEV_Module_Exit();
 }
 
-void drawNumberBox(UWORD x, UWORD y, UWORD boxSize, int number, sFONT* font) {
-    char buf[4];
-    snprintf(buf, sizeof(buf), "%d", number);
-    Paint_DrawRectangle(x, y, x + boxSize, y + boxSize, BLACK, 2, DRAW_FILL_EMPTY);
-    Paint_DrawString_EN(x, y + 5, buf, font, WHITE, BLACK);
-}
-
-void drawShoeSizes(UBYTE *BlackImage, int numbers[], int stockNumbers[], int numBoxes) {
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(WHITE);
-
-    // Constants
-    const UWORD titleHeight = 30;
-    const UWORD boxSize = 35;
-    const UWORD xStart = 3;
-    UWORD y = titleHeight + 10;
-
-    // Draw title bar
-    Paint_DrawRectangle(0, 0, EPD_2in13_V3_HEIGHT, titleHeight, BLACK, 2, DRAW_FILL_EMPTY);
-    Paint_DrawString_EN(5, 5, "Cool Shoe", &Font24, WHITE, BLACK);
-
-    // Draw sizes
-    UWORD x = xStart;
-    for(int i = 0; i < numBoxes; i++) {
-        drawNumberBox(x, y, boxSize, numbers[i], &Font24);
-        x += boxSize;
-    }
-
-    // Draw stock numbers below sizes
-    y += boxSize;
-    x = xStart;
-    for(int i = 0; i < numBoxes; i++) {
-        drawNumberBox(x, y, boxSize, stockNumbers[i], &Font24);
-        x += boxSize;
-    }
+#define TOTAL_BOXES 7
+#define EPD_WIDTH EPD_2in13_V3_HEIGHT
+#define BOX_GAP 2
+#define BOX_SIZE ((EPD_WIDTH - (BOX_GAP * (TOTAL_BOXES - 1))) / TOTAL_BOXES)
+// Draw the title bar
+void drawTitle(const char *title) {
+    Paint_DrawRectangle(0, 0, EPD_2in13_V3_HEIGHT, 30, BLACK, 2, DRAW_FILL_FULL);
+    Paint_DrawString_EN(5, 5, title, &Font24, BLACK, WHITE);
 }
 
 
 
+typedef enum {
+    STYLE_DEFAULT,
+    STYLE_INVERTED,
+    STYLE_INVERTED_STRIKE,
+} NumberBoxStyle;
+
+typedef struct {
+    UWORD number;
+    NumberBoxStyle style;
+} NumberBox_t;
+
+void drawNumberBox(UWORD x, UWORD y, NumberBox_t *numberbox){
+    assert(numberbox->number >= 0 && numberbox->number <= 99);
+    assert((x + BOX_SIZE) <= EPD_2in13_V3_HEIGHT);
+    assert((y + BOX_SIZE) <= EPD_2in13_V3_WIDTH);
+
+    const UWORD rectXEnd = x + BOX_SIZE - 1;
+    const UWORD rectYEnd = y + BOX_SIZE - 1;
+    const int num_digits = (numberbox->number > 9) ? 2 : 1;
+    const int totalTextWidth = num_digits * 17;
+    const int totalTextHeight = 22;
+    UWORD textX = (x + (BOX_SIZE - totalTextWidth) / 2);
+    const UWORD textY = y + (BOX_SIZE - totalTextHeight) / 2;
+
+    int foreground, background, fill, fill_colour, stroke_width;
+    switch(numberbox->style) {
+        case STYLE_INVERTED_STRIKE:
+            Paint_DrawLine(x, y, rectXEnd, rectYEnd, BLACK, 2, LINE_STYLE_SOLID);
+        case STYLE_INVERTED:
+            foreground = BLACK;
+            background = WHITE;
+            fill = DRAW_FILL_EMPTY;
+            fill_colour = foreground;
+            stroke_width = 2;
+            break;
+        default:  // STYLE_DEFAULT
+            foreground = WHITE;
+            background = BLACK;
+            fill = DRAW_FILL_FULL;
+            fill_colour = background;
+            stroke_width = 1;
+            y += 1;
+            textX -= 1;
+    }
+
+    Paint_DrawRectangle(x, y, rectXEnd, rectYEnd, fill_colour, stroke_width, fill);
+    Paint_DrawNum(textX, textY, numberbox->number, &Font24, foreground, background);
+}
+
+void drawNumberBoxes(UBYTE *BlackImage, NumberBox_t numbers[], int numBoxes, const UWORD yOffset) {
+    assert(numBoxes <= TOTAL_BOXES);
+    for (UWORD x = 0, i = 0; i < numBoxes; ++i) {
+        drawNumberBox(x, yOffset, &numbers[i]);
+        x += (BOX_SIZE + BOX_GAP);
+    }
+}
 
 int EPD_2in13_V3_test(void) {
     initEPD();
     measureClearTime();
     UBYTE *BlackImage = createAndConfigImage();
-    // drawOnImage(BlackImage);
+    drawTitle("Oboz Bridger");
 
-    int sizes[] = {8, 9, 10, 11, 12, 13, 14};
-    int stockNumbers[] = {0, 2, 5, 3, 4, 1, 5}; // Example stock numbers, you can populate this differently
-    int numBoxes = sizeof(sizes) / sizeof(sizes[0]);  // Calculate the number of elements in the array
+    NumberBox_t sizes[] = {
+        {.style = STYLE_DEFAULT, .number = 8},
+        {.style = STYLE_INVERTED, .number = 9},
+        {.style = STYLE_INVERTED_STRIKE, .number = 10},
+        {.style = STYLE_DEFAULT, .number = 11},
+        {.style = STYLE_DEFAULT, .number = 12},
+        {.style = STYLE_DEFAULT, .number = 13},
+        {.style = STYLE_INVERTED_STRIKE, .number = 14},
+    };
 
-    // Draw shoe sizes
-    drawShoeSizes(BlackImage, sizes, stockNumbers, numBoxes);
+    int numBoxes = ARRAY_SIZE(sizes);  // Using the macro here
+    Paint_DrawString_EN(5, 35, "$260.00", &Font24, WHITE, BLACK);
+    Paint_DrawString_EN(5, 60, "Best seller!", &Font24, WHITE, BLACK);
 
-    // // Draw stock numbers
-    // drawShoeSizes(BlackImage, stockNumbers, numBoxes);
-
+    drawNumberBoxes(BlackImage, sizes, numBoxes, 88);
     EPD_2in13_V3_Display_Base(BlackImage);
     finalizeEPD(BlackImage);
 
