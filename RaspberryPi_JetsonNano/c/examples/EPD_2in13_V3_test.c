873f2a7282530014303db55dc23aa143a8638c32
Gavin Guinn <gavinguinn1@gmail.com>
2023-09-10 04:33:53 +0100

    draw shoe boxes

--- RaspberryPi_JetsonNano/c/examples/EPD_2in13_V3_test.c
+++ RaspberryPi_JetsonNano/c/examples/EPD_2in13_V3_test.c
@@ -31,140 +31,111 @@
 #include "EPD_2in13_V3.h"
 #include <time.h> 
 
-int EPD_2in13_V3_test(void)
-{
+
+void initEPD() {
     Debug("EPD_2in13_V3_test Demo\r\n");
-    if(DEV_Module_Init()!=0){
-        return -1;
+    if(DEV_Module_Init() != 0) {
+        exit(-1); // exit if initialization fails
     }
-
     Debug("e-Paper Init and Clear...\r\n");
-	EPD_2in13_V3_Init();
+    EPD_2in13_V3_Init();
+}
 
-	struct timespec start={0,0}, finish={0,0}; 
-    clock_gettime(CLOCK_REALTIME,&start);
+void measureClearTime() {
+    struct timespec start = {0,0}, finish = {0,0};
+    clock_gettime(CLOCK_REALTIME, &start);
     EPD_2in13_V3_Clear();
-	clock_gettime(CLOCK_REALTIME,&finish);
-    Debug("%ld S\r\n",finish.tv_sec-start.tv_sec);	
+    clock_gettime(CLOCK_REALTIME, &finish);
+    Debug("%ld S\r\n", finish.tv_sec - start.tv_sec);
+}
 
-    //Create a new image cache
-    UBYTE *BlackImage;
-    UWORD Imagesize = ((EPD_2in13_V3_WIDTH % 8 == 0)? (EPD_2in13_V3_WIDTH / 8 ): (EPD_2in13_V3_WIDTH / 8 + 1)) * EPD_2in13_V3_HEIGHT;
-    if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
+UBYTE* createAndConfigImage() {
+    UWORD Imagesize = ((EPD_2in13_V3_WIDTH % 8 == 0) ? (EPD_2in13_V3_WIDTH / 8) : (EPD_2in13_V3_WIDTH / 8 + 1)) * EPD_2in13_V3_HEIGHT;
+    UBYTE *BlackImage = (UBYTE *)malloc(Imagesize);
+    if(BlackImage == NULL) {
         Debug("Failed to apply for black memory...\r\n");
-        return -1;
+        exit(-1);
     }
-    Debug("Paint_NewImage\r\n");
     Paint_NewImage(BlackImage, EPD_2in13_V3_WIDTH, EPD_2in13_V3_HEIGHT, 90, WHITE);
-	Paint_Clear(WHITE);
-	
-#if 1   // show bmp
-	Paint_NewImage(BlackImage, EPD_2in13_V3_WIDTH, EPD_2in13_V3_HEIGHT, 90, WHITE);
-    Debug("show window BMP-----------------\r\n");
-    Paint_SelectImage(BlackImage);
-    GUI_ReadBmp("./pic/100x100.bmp", 10, 10);
-    EPD_2in13_V3_Display(BlackImage);
-    DEV_Delay_ms(3000);
-
-    Debug("show bmp------------------------\r\n");
-    Paint_SelectImage(BlackImage);
-    GUI_ReadBmp("./pic/2in13_1.bmp", 0, 0);
-    EPD_2in13_V3_Display(BlackImage);
-    DEV_Delay_ms(3000);
-#endif
-
-#if 1   //show image for array    
-    Debug("show image for array\r\n");
-    Paint_SelectImage(BlackImage);
     Paint_Clear(WHITE);
-    Paint_DrawBitMap(gImage_2in13_2);
+    return BlackImage;
+}
 
-    EPD_2in13_V3_Display(BlackImage);
-    DEV_Delay_ms(2000);
-#endif
-
-#if 1  // Drawing on the image
-	Paint_NewImage(BlackImage, EPD_2in13_V3_WIDTH, EPD_2in13_V3_HEIGHT, 90, WHITE);  	
+void drawOnImage(UBYTE *BlackImage) {
     Debug("Drawing\r\n");
-    //1.Select Image
     Paint_SelectImage(BlackImage);
     Paint_Clear(WHITE);
-	
-    // 2.Drawing on the image
-    Paint_DrawPoint(5, 10, BLACK, DOT_PIXEL_1X1, DOT_STYLE_DFT);
-    Paint_DrawPoint(5, 25, BLACK, DOT_PIXEL_2X2, DOT_STYLE_DFT);
-    Paint_DrawPoint(5, 40, BLACK, DOT_PIXEL_3X3, DOT_STYLE_DFT);
-    Paint_DrawPoint(5, 55, BLACK, DOT_PIXEL_4X4, DOT_STYLE_DFT);
+    Paint_DrawNum(0, 10, 123456789, &Font24, WHITE, BLACK);
+}
 
-    Paint_DrawLine(20, 10, 70, 60, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
-    Paint_DrawLine(70, 10, 20, 60, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
-    Paint_DrawRectangle(20, 10, 70, 60, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-    Paint_DrawRectangle(85, 10, 135, 60, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-
-    Paint_DrawLine(45, 15, 45, 55, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-    Paint_DrawLine(25, 35, 70, 35, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-    Paint_DrawCircle(45, 35, 20, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-    Paint_DrawCircle(110, 35, 20, WHITE, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-
-    Paint_DrawString_EN(140, 15, "waveshare", &Font16, BLACK, WHITE);
-    Paint_DrawNum(140, 40, 123456789, &Font16, BLACK, WHITE);
-
-    Paint_DrawString_CN(140, 60, "ÄãºÃabc", &Font12CN, BLACK, WHITE);
-    Paint_DrawString_CN(5, 65, "Î¢Ñ©µç×Ó", &Font24CN, WHITE, BLACK);
-
-    EPD_2in13_V3_Display_Base(BlackImage);
-    DEV_Delay_ms(3000);
-#endif
-
-#if 1   //Partial refresh, example shows time
-	Paint_NewImage(BlackImage, EPD_2in13_V3_WIDTH, EPD_2in13_V3_HEIGHT, 90, WHITE);  
-    Debug("Partial refresh\r\n");
-    Paint_SelectImage(BlackImage);
-	
-    PAINT_TIME sPaint_time;
-    sPaint_time.Hour = 12;
-    sPaint_time.Min = 34;
-    sPaint_time.Sec = 56;
-    UBYTE num = 10;
-    for (;;) {
-        sPaint_time.Sec = sPaint_time.Sec + 1;
-        if (sPaint_time.Sec == 60) {
-            sPaint_time.Min = sPaint_time.Min + 1;
-            sPaint_time.Sec = 0;
-            if (sPaint_time.Min == 60) {
-                sPaint_time.Hour =  sPaint_time.Hour + 1;
-                sPaint_time.Min = 0;
-                if (sPaint_time.Hour == 24) {
-                    sPaint_time.Hour = 0;
-                    sPaint_time.Min = 0;
-                    sPaint_time.Sec = 0;
-                }
-            }
-        }
-        Paint_ClearWindows(150, 80, 150 + Font20.Width * 7, 80 + Font20.Height, WHITE);
-        Paint_DrawTime(150, 80, &sPaint_time, &Font20, WHITE, BLACK);
-
-        num = num - 1;
-        if(num == 0) {
-            break;
-        }
-		EPD_2in13_V3_Display_Partial(BlackImage);
-        DEV_Delay_ms(500);//Analog clock 1s
-    }
-#endif
-
-	Debug("Clear...\r\n");
-	EPD_2in13_V3_Init();
-    EPD_2in13_V3_Clear();
-	
+void finalizeEPD(UBYTE *BlackImage) {
     Debug("Goto Sleep...\r\n");
     EPD_2in13_V3_Sleep();
     free(BlackImage);
     BlackImage = NULL;
-    DEV_Delay_ms(2000);//important, at least 2s
-    // close 5V
+    DEV_Delay_ms(2000);
     Debug("close 5V, Module enters 0 power consumption ...\r\n");
     DEV_Module_Exit();
-    return 0;
 }
 
+void drawNumberBox(UWORD x, UWORD y, UWORD boxSize, int number, sFONT* font) {
+    char buf[4];
+    snprintf(buf, sizeof(buf), "%d", number);
+    Paint_DrawRectangle(x, y, x + boxSize, y + boxSize, BLACK, 2, DRAW_FILL_EMPTY);
+    Paint_DrawString_EN(x, y + 5, buf, font, WHITE, BLACK);
+}
+
+void drawShoeSizes(UBYTE *BlackImage, int numbers[], int stockNumbers[], int numBoxes) {
+    Paint_SelectImage(BlackImage);
+    Paint_Clear(WHITE);
+
+    // Constants
+    const UWORD titleHeight = 30;
+    const UWORD boxSize = 35;
+    const UWORD xStart = 3;
+    UWORD y = titleHeight + 10;
+
+    // Draw title bar
+    Paint_DrawRectangle(0, 0, EPD_2in13_V3_HEIGHT, titleHeight, BLACK, 2, DRAW_FILL_EMPTY);
+    Paint_DrawString_EN(5, 5, "Cool Shoe", &Font24, WHITE, BLACK);
+
+    // Draw sizes
+    UWORD x = xStart;
+    for(int i = 0; i < numBoxes; i++) {
+        drawNumberBox(x, y, boxSize, numbers[i], &Font24);
+        x += boxSize;
+    }
+
+    // Draw stock numbers below sizes
+    y += boxSize;
+    x = xStart;
+    for(int i = 0; i < numBoxes; i++) {
+        drawNumberBox(x, y, boxSize, stockNumbers[i], &Font24);
+        x += boxSize;
+    }
+}
+
+
+
+
+int EPD_2in13_V3_test(void) {
+    initEPD();
+    measureClearTime();
+    UBYTE *BlackImage = createAndConfigImage();
+    // drawOnImage(BlackImage);
+
+    int sizes[] = {8, 9, 10, 11, 12, 13, 14};
+    int stockNumbers[] = {0, 2, 5, 3, 4, 1, 5}; // Example stock numbers, you can populate this differently
+    int numBoxes = sizeof(sizes) / sizeof(sizes[0]);  // Calculate the number of elements in the array
+
+    // Draw shoe sizes
+    drawShoeSizes(BlackImage, sizes, stockNumbers, numBoxes);
+
+    // // Draw stock numbers
+    // drawShoeSizes(BlackImage, stockNumbers, numBoxes);
+
+    EPD_2in13_V3_Display_Base(BlackImage);
+    finalizeEPD(BlackImage);
+
+    return 0;
+}
